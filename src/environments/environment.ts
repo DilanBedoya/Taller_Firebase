f684bf238733c4b94705ca5a9053354ae0908ea2
AleXCS003 <alexd003cardenas@gmail.com>
2024-11-11 17:49:15 -0500

    Taller Acabado

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,6 +4,16 @@
 
 export const environment = {
   production: false,
+  // firebase: {
+  //   apiKey: "AIzaSyAuKtCEdBHIp1OPAchWLPwBJD94xGEz9bw",
+  //   authDomain: "b-dfe16.firebaseapp.com",
+  //   projectId: "b-dfe16",
+  //   storageBucket: "b-dfe16.firebasestorage.app",
+  //   messagingSenderId: "167089758686",
+  //   appId: "1:167089758686:web:c94d24ca8d8a3e920e7d4f",
+  //   measurementId: "G-LTD6KKYYGM"
+  // }
+  //credenciales inge
   firebase: {
     apiKey: "AIzaSyDp4RCs8UwhBrTNivTVoBkbDwQ_s1LYxLI",
     authDomain: "b-1accc.firebaseapp.com",
