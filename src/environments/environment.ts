702cea1f9dde79397c8c34c799ceeae87fb9010c
DilanBedoya <estilius2@hotmail.com>
2024-11-12 10:45:19 -0500

    Correccion firebase

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,25 +4,8 @@
 
 export const environment = {
   production: false,
-  // firebase: {
-  //   apiKey: "AIzaSyAuKtCEdBHIp1OPAchWLPwBJD94xGEz9bw",
-  //   authDomain: "b-dfe16.firebaseapp.com",
-  //   projectId: "b-dfe16",
-  //   storageBucket: "b-dfe16.firebasestorage.app",
-  //   messagingSenderId: "167089758686",
-  //   appId: "1:167089758686:web:c94d24ca8d8a3e920e7d4f",
-  //   measurementId: "G-LTD6KKYYGM"
-  // }
-  //credenciales inge
-  firebase: {
-    apiKey: "AIzaSyDp4RCs8UwhBrTNivTVoBkbDwQ_s1LYxLI",
-    authDomain: "b-1accc.firebaseapp.com",
-    projectId: "b-1accc",
-    storageBucket: "b-1accc.firebasestorage.app",
-    messagingSenderId: "606986248368",
-    appId: "1:606986248368:web:df6c0a16fa5bc4ed9a9b75",
-    measurementId: "G-QDN8GEEH17"
-  }
+
+
 };
 
 /*
